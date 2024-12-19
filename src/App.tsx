e123d22c9f4bc90d8735e58a8f55583ee9288c5e
Abdulquadri Jinad <abdjinadquadri@gmail.com>
2024-12-19 15:37:53 +0100

    feat: made responsive for desktop screens

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,13 @@ function Home() {
   const handlePlayerChoice = (choice: Choice) => {
     setPlayerChoice(choice);
 
-    // Simulate house picking after 2 seconds
     setTimeout(() => {
       const housePick = getRandomChoice();
       setHouseChoice(housePick);
 
-      // Determine winner
       const gameWinner = determineWinner(choice, housePick);
       setWinner(gameWinner);
 
-      // Update score
       if (gameWinner === "player") setScore((prev) => prev + 1);
       if (gameWinner === "house") setScore((prev) => (prev > 0 ? prev - 1 : 0));
     }, 1_000);
@@ -41,12 +38,12 @@ function Home() {
   return (
     <div className="min-h-screen flex flex-col text-gray-100 justify-around items-center">
       {/* Header */}
-      <header className="border-4 mb-6 w-full max-w-3xl mx-auto flex justify-between items-center p-4 rounded-lg border-gray-500 ">
+      <header className="w-full md:mx-6 border-4 mb-6   max-w-3xl mx-auto flex justify-between items-center p-4 rounded-lg border-gray-500 ">
         <h1 className="w-1/2 text-left leading-none font-extrabold text-3xl break-words">
           Rock <br /> Paper <br /> Scissors
         </h1>
 
-        <div className="py-2 px-6 bg-white rounded-lg">
+        <div className="py-2 px-6  rounded-lg bg-white">
           <h4 className="text-gray-700 uppercase font-extrabold text-xs">
             Score
           </h4>
@@ -56,36 +53,36 @@ function Home() {
 
       {/* Game Board */}
       {!playerChoice ? (
-        <section className="relative">
+        <section className="relative w-full md:w-3/5  my-12">
           <img src={BgTriangle} alt="" className="w-full h-auto" />
 
           <div
-            className="bg-white h-28 w-28 cursor-pointer hover:shadow-xl hover:scale(110) shadow-md border-8 border-[#4464F9] flex items-center justify-center rounded-full absolute -top-9 left-0"
+            className="bg-white h-28 w-28 md:h-40 md:w-40 cursor-pointer hover:shadow-xl md:-left-6 hover:scale(110) shadow-md border-8 border-[#4464F9] flex items-center justify-center rounded-full absolute -top-9 left-0"
             onClick={() => handlePlayerChoice("rock")}
           >
             <img src={IconRock} alt="Rock" />
           </div>
 
           <div
-            className="bg-white h-28 w-28 cursor-pointer hover:shadow-xl hover:scale(110) shadow-md border-8 border-[#ECA31B] flex items-center justify-center rounded-full absolute -top-9 right-0"
+            className="bg-white h-28 w-28 md:h-40 md:w-40 cursor-pointer hover:shadow-xl md:-right-6 hover:scale(110) shadow-md border-8 border-[#ECA31B] flex items-center justify-center rounded-full absolute -top-9 right-0"
             onClick={() => handlePlayerChoice("paper")}
           >
             <img src={IconPaper} alt="Paper" />
           </div>
 
           <div
-            className="bg-white h-28 w-28 cursor-pointer hover:shadow-xl hover:scale(110) shadow-md border-8 border-[#DD3754] flex items-center justify-center rounded-full absolute bottom-0 left-1/2 -translate-x-1/2"
+            className="bg-white h-28 w-28 md:h-40 md:w-40 cursor-pointer hover:shadow-xl hover:scale(110) shadow-md border-8 border-[#DD3754] flex items-center justify-center rounded-full absolute bottom-0 left-1/2 -translate-x-1/2"
             onClick={() => handlePlayerChoice("scissors")}
           >
             <img src={IconScissors} alt="Scissors" />
           </div>
         </section>
       ) : (
-        <section className="flex justify-between w-full gap-4 items-center">
+        <section className="flex justify-between md:gap-8 lg:w-1/2 lg:mx-auto w-full gap-4 items-center">
           {/* Player Choice */}
           <div>
             <div
-              className={`bg-white h-28 w-28 shadow-md border-8 border-[#4464F9] flex items-center justify-center rounded-full`}
+              className={`bg-white h-28 w-28 md:h-40 md:w-40 lg:w-48 lg:h-48 shadow-md border-8 border-[#4464F9] flex items-center justify-center rounded-full`}
             >
               <img
                 src={
@@ -109,7 +106,7 @@ function Home() {
               <div className="bg-black/30 h-28 w-28 shadow-md rounded-full animate-pulse"></div>
             ) : (
               <div
-                className={`bg-white h-28 w-28 shadow-md border-8 border-[#ECA31B] flex items-center justify-center rounded-full`}
+                className={`bg-white  h-28 w-28 md:h-40 md:w-40 lg:w-48 lg:h-48 shadow-md border-8 border-[#ECA31B] flex items-center justify-center rounded-full`}
               >
                 <img
                   src={
@@ -154,9 +151,10 @@ function Home() {
           </button>
         </div>
       )}
+
       <a
         href="/rules"
-        className="text-gray-200 cursor-pointer py-3 px-8 border border-gray-400 rounded-md"
+        className="text-gray-200 cursor-pointer lg:self-end py-3 px-8 border border-gray-400 rounded-md"
       >
         Rules
       </a>
